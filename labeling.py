741e095e38acda225791afbfc453c38e918725e3
tnwlsgk1002 <76458724+tnwlsgk1002@users.noreply.github.com>
2021-01-07 18:39:18 +0900

    이미지 이동, 삭제 완료, 그리기 미흡
    그리기 과정에서 원하는 위치에서 그려지지 않는 문제 미해결

--- labeling.py
+++ labeling.py
@@ -3,8 +3,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import cv2
-import os # 확장자 파일 가져오기
+import os  # 확장자 파일 가져오기
 import copy
+import natsort
+
 
 class MyApp(QWidget):
     def __init__(self):
@@ -13,13 +15,13 @@ class MyApp(QWidget):
 
     def initUI(self):
         self.setWindowTitle('labeling program')
-        self.setGeometry(300, 300, 850, 500)
+        self.setGeometry(300, 300, 500, 400)
 
         # 라벨
         self.label_list = []
         self.label = Label(self)
 
-        #SetCursor 설정
+        # SetCursor 설정
         Cursor = QCursor(QPixmap('cross.png'))
         self.label.setCursor(Cursor)
 
@@ -52,6 +54,7 @@ class MyApp(QWidget):
         self.rightbtn.setShortcut("Right")
         self.leftbtn.clicked.connect(self.movebtnClicked)
         self.rightbtn.clicked.connect(self.movebtnClicked)
+
         # 레이아웃 설정
         vbox = QVBoxLayout()
 
@@ -80,11 +83,13 @@ class MyApp(QWidget):
             self.dir.setText(self.folder)
             file_list = os.listdir(self.folder)
             self.label_list = [file for file in file_list if file.endswith(".png") or file.endswith(".jpg")]
+            self.label_list = natsort.natsorted(self.label_list)
             if not self.label_list:
-               return
-            self.file = self.folder+"/"+self.label_list[0]
+                return
+            self.total = len(self.label_list)
+            self.file = self.folder + "/" + self.label_list[0]
             self.n1 = 0
-            self.label.createPixmap()
+            self.label.initPixmap()
 
         except FileNotFoundError:
             return
@@ -100,14 +105,17 @@ class MyApp(QWidget):
         if not self.label_list:
             return
         sender = self.sender()
-        self.label.text_upload()
+        self.label.text_save()
         if sender == self.leftbtn:
-            self.n1 = self.n1-1
-        else:
-            self.n1 = self.n1+1
+            self.n1 = self.n1 - 1
+            if self.n1 == -1:
+                self.n1 = len(self.label_list) - 1
+        elif sender == self.rightbtn:
+            self.n1 = self.n1 + 1
+            if self.n1 == len(self.label_list):
+                self.n1 = 0
         self.file = self.folder + "/" + self.label_list[self.n1]
         self.label.createPixmap()
-
 
 
 class Label(QLabel):
@@ -115,30 +123,113 @@ class Label(QLabel):
         super().__init__()
         self.setParent(parent)
         self.drawing = False
+        self.removing = False
         self.exist = False
         self.ix = None
         self.iy = None
-        self.n2 = 0
-        self.text = [[-1 for j in range(10)] for i in range(10)] # 0으로 채워진 3차원 리스트
+        self.ori_image = []
+        self.text = []
 
-    def createPixmap(self):
+    def initPixmap(self):
         self.image = cv2.imread(self.parent().file, cv2.IMREAD_COLOR)
+        image = copy.deepcopy(self.image)
+        self.ori_image.append(image)
         self.h, self.w = self.image.shape[:2]
         qimage = QImage(self.image.data, self.w, self.h, self.image.strides[0], QImage.Format_BGR888)
         self.setPixmap(QPixmap.fromImage(qimage))
         self.exist = True
+        self.text.append([0])
+        self.resize(self.w, self.h)
+        self.parent().resize(self.w + 200, self.h + 200)
+
+    # 텍스트를 기반으로 setPixmap (이동, 삭제 시)
+    def createPixmap(self):
+        print("\n=====createPixmap=====")
+        print("self.parent().n1:", self.parent().n1)
+
+        # 처음 이미지를 열었을 경우
+        if len(self.text) < self.parent().n1 + 1:
+            print("이미지를 부른 적이 없음")
+            self.initPixmap()
+            return
+
+        print("text[n1]: ", self.text[self.parent().n1])
+
+        # 이미지에 바운딩박스가 없는 경우
+        if self.text[self.parent().n1] == [0]:
+            print('이미지를 불러봤으나 바운딩박스가 없음')
+            self.image = copy.deepcopy(self.ori_image[self.parent().n1])
+            print('d')
+            self.h, self.w = self.image.shape[:2]
+            qimage = QImage(self.image.data, self.w, self.h, self.image.strides[0], QImage.Format_BGR888)
+            print('s')
+            self.setPixmap(QPixmap.fromImage(qimage))
+            print('e')
+            self.update()
+            self.resize(self.w, self.h)
+            print('aa')
+            return
+
+        print("self.text[n1]: ", self.text[self.parent().n1])
+        self.image = copy.deepcopy(self.ori_image[self.parent().n1])
+        for i in self.text[self.parent().n1]:
+            if i[4] == 'Dog':
+                self.image = cv2.rectangle(self.image, (i[0], i[1]), (i[2], i[3]), (0, 0, 255))
+                self.image = cv2.putText(self.image, 'Dog', ((i[0] + i[2]) // 2, i[1] - 10),
+                                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            elif i[4] == 'Cat':
+                self.image = cv2.rectangle(self.image, (i[0], i[1]), (i[2], i[3]), (255, 0, 0))
+                self.image = cv2.putText(self.image, 'Cat', ((i[0] + i[2]) // 2, i[1] - 10),
+                                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+        self.h, self.w = self.image.shape[:2]
+        qimage = QImage(self.image.data, self.w, self.h, self.image.strides[0], QImage.Format_BGR888)
+        self.setPixmap(QPixmap.fromImage(qimage))
+        self.update()
+        self.resize(self.w, self.h)
 
     def mousePressEvent(self, e):
-        if not self.exist :
+        if not self.exist:
             return
+
         if e.button() == Qt.LeftButton:
             self.ix = e.x()
             self.iy = e.y()
             self.drawing = True
+            return
+
+        # 삭제
+        if e.button() == Qt.RightButton:
+            remove_index = self.box_exist(e.x(), e.y())
+            print('\nremove_index: ', remove_index)
+            if remove_index == -1:
+                return
+            # text에서 바운딩 박스 삭제
+            # text를 바탕으로 qimage draw, self.image에 적용
+            if len(self.text[self.parent().n1]) == 1:
+                self.text[self.parent().n1].pop(remove_index)
+                self.createPixmap()
+                self.text[self.parent().n1].append(0)
+                return
+
+            self.text[self.parent().n1].pop(remove_index)
+            self.createPixmap()
+
+            return
+
+    # text 안에 바운딩 박스가 있는가 확인
+    def box_exist(self, x, y):
+        for i in self.text[self.parent().n1]:
+            if i == -1:
+                break
+            if (i[0] <= x and i[1] <= y and i[2] >= x and i[3] >= y) or (
+                    i[0] >= x and i[1] >= y and i[2] <= x and i[3] <= y):
+                return self.text[self.parent().n1].index(i)
+        return -1
 
     def mouseMoveEvent(self, e):
-        if not self.exist :
+        if not self.exist:
             return
+
         if self.drawing:
             qimage = QImage(self.image.data, self.w, self.h, self.image.strides[0], QImage.Format_BGR888)
             self.setPixmap(QPixmap.fromImage(qimage))
@@ -146,39 +237,52 @@ class Label(QLabel):
             img = copy.deepcopy(self.image)
             if self.parent().animalType == 0:
                 img = cv2.rectangle(img, (self.ix, self.iy), (e.x(), e.y()), (0, 0, 255))
-            else :
+            else:
                 img = cv2.rectangle(img, (self.ix, self.iy), (e.x(), e.y()), (255, 0, 0))
             qimage = QImage(img.data, self.w, self.h, img.strides[0], QImage.Format_BGR888)
             self.setPixmap(QPixmap.fromImage(qimage))
             self.repaint()
 
-
     def mouseReleaseEvent(self, e):
-        if not self.exist :
+
+        # 삭제 시 그리기 방지
+        if e.button() == Qt.RightButton:
             return
+
+        if not self.exist:
+            return
+
         self.drawing = False
         if self.parent().animalType == 0:
             self.image = cv2.rectangle(self.image, (self.ix, self.iy), (e.x(), e.y()), (0, 0, 255))
-            self.image = cv2.putText(self.image, 'Dog', ((self.ix+e.x())//2, self.iy-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            self.image = cv2.putText(self.image, 'Dog', ((self.ix + e.x()) // 2, self.iy - 10),
+                                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
             list = [self.ix, self.iy, e.x(), e.y(), 'Dog']
         else:
             self.image = cv2.rectangle(self.image, (self.ix, self.iy), (e.x(), e.y()), (255, 0, 0))
-            self.image = cv2.putText(self.image, 'Cat', ((self.ix+e.x())//2, self.iy-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            self.image = cv2.putText(self.image, 'Cat', ((self.ix + e.x()) // 2, self.iy - 10),
+                                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
             list = [self.ix, self.iy, e.x(), e.y(), 'Cat']
-        self.text[self.parent().n1][self.n2] = list
-        self.n2 = self.n2 + 1
+
+        self.text[self.parent().n1].append(list)
+        if self.text[self.parent().n1][0] == 0:  # 첫 바운딩 박스인 경우
+            del self.text[self.parent().n1][0]
+        print("========그리기 =======")
+        print("self.text : ", self.text)
         qimage = QImage(self.image.data, self.w, self.h, self.image.strides[0], QImage.Format_BGR888)
         self.setPixmap(QPixmap.fromImage(qimage))
 
-    def text_upload(self):
+    # 텍스트파일을 저장하여 업로드
+    def text_save(self):
         fname = self.parent().file.split('.')
-        with open(fname[0]+".txt", 'w') as file :
+        print(self.text)
+        with open(fname[0] + ".txt", 'w') as file:
             for i in self.text:
                 for j in i:
-                    if j == -1:
+                    if j == 0:
                         return
                     for k in j:
-                        file.write(str(k)+' ')
+                        file.write(str(k) + ' ')
                     file.write('\n')
 
 
